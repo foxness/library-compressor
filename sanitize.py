08b40ce31d07f92b0ca6e8535c5429a491e3c83b
River <foxnezz@gmail.com>
2025-12-22 00:09:00 +0300

    [FIX] fix santizer crash on no metadata file

--- sanitize.py
+++ sanitize.py
@@ -6,11 +6,11 @@ import threading
 import queue
 import time
 
-input_dir = '/Volumes/Athena/river-lib/small_lib'
+input_dir = '/Volumes/Athena/river-lib/huge_jpg_lib'
 
 worker_count = 8
 converted_extensions = ['avif', 'jxl', 'webp']
-valid_extensions = ['png', 'jpg', 'jpeg', 'gif']
+valid_extensions = ['jpg', 'jpeg']
 
 print_log_lock = threading.Lock()
 
@@ -28,7 +28,14 @@ def purge(image_dir):
 
 def process_one(dir_path, index, total_count, name):
     files = [f.path for f in os.scandir(dir_path) if not f.is_dir()]
-    metadata_file = [a for a in files if os.path.basename(a) == 'metadata.json'][0]
+    metadata_file = [a for a in files if os.path.basename(a) == 'metadata.json']
+    if not metadata_file:
+        safe_print(f'[{name}] {dir_path} no metadata file, purging')
+        result = purge(dir_path)
+        return
+
+    metadata_file = metadata_file[0]
+
     with open(metadata_file, 'r') as file:
         metadata = json.load(file)
 
@@ -43,6 +50,11 @@ def process_one(dir_path, index, total_count, name):
 
     if extension not in valid_extensions:
         safe_print(f'[{name}] {extension} is not a valid extension, purging')
+        is_bad = True
+
+    image_exists = os.path.isfile(os.path.join(dir_path, image_name))
+    if not image_exists:
+        safe_print(f'[{name}] {image_name} does not exist, purging')
         is_bad = True
 
     if is_bad:
